443d80705300bddea6390d0748a7c1d9cd661fb9
yeah_hu <yeah_hu@yeah.net>
2014-03-26 11:47:10 +0800

    fix bug

--- session/js/generateSession.js
+++ session/js/generateSession.js
@@ -1,5 +1,142 @@
+/*
+ * global variables
+ */
+//time_out = 1000 * 60 * 5;
+//time_out_cps = 60 * 60 * 24;
 authenticationUrl="http://localhost/session/page";
 
+var SessionObj = new Object();
+SessionObj.Session_ID = getCookie("Session_ID");
+SessionObj.Token_ID = getCookie("Token_ID");
+SessionObj.Token_Date = getCookie("Token_Date");
+SessionObj.User_ID = getCookie("USER_ID");
+SessionObj.IsLinkSuccess = true;
+/*
+SessionObj.User_Name = getCookie("COOKIE_USER_NAME");
+SessionObj.NickName = getCookie("COOKIE_NICKNAME");
+SessionObj.User_Level_ID = getCookie("COOKIE_USER_LEVEL_ID");
+SessionObj.Token_Date = getCookie("COOKIE_TOKEN_DATE");
+SessionObj.User_IP = getCookie("COOKIE_USER_IP");
+SessionObj.User_City = getCookie("COOKIE_USER_CITY");
+SessionObj.linkData="";
+*/
+
+
+if (SessionObj.Session_ID == null || SessionObj.Session_ID == ""
+		|| SessionObj.Token_ID  == null || SessionObj.Token_ID  == ""
+		|| SessionObj.Token_Date == null || SessionObj.Token_Date == ""
+/*		|| SessionObj.User_IP == null || SessionObj.User_IP == ""
+		|| SessionObj.User_Level_ID == null || SessionObj.User_Level_ID == ""
+		|| bNeedExtend*/
+		) {
+	SessionObj.IsLinkSuccess = false;
+
+	//JSONP
+	//generateSessionJSONP(authenticationUrl+"/api/list/generateSession.jsonp?IS_PHONE=0&a="+ Math.random());
+	//generateSessionJSONP(authenticationUrl+"/generateSessionJSONP.php?a="+ Math.random());
+	generateSessionJSONP(authenticationUrl+"/generateSessionJSONP.php?");
+}
+
+/*
+ * cps 
+ */
+
+//record cps
+var newCPSID ="";
+var oldCPS = getCookie("CPS_ID");
+
+if(getParameterByName("cps_id")){
+	//cps_id found in url
+	newCPSID = getParameterByName("cps_id");
+	newCPSID="C_"+newCPSID;
+}else if(document.referrer && (!oldCPS || oldCPS.startWith("W_"))){
+	//cps_id not found in url
+	newCPSID="W_"+document.referrer.replace("http://", "");
+	if(newCPSID.indexOf("/")>0)
+		newCPSID=newCPSID.split("/")[0];
+	//if(newCPSID.indexOf("localhost")>0)
+		//newCPSID=null;
+}
+
+if(newCPSID){
+	if(oldCPS != newCPSID){
+		//获取当前时间
+		date=new Date();
+		expireDays=1;
+		//将date设置为1天以后的时间
+		date.setTime(date.getTime()+expireDays*24*3600*1000);
+		timeout = date.toGMTString();
+		//将cookie设置为1天后过期
+		setCookie_hy("CPS_ID",newCPSID,timeout);
+	}
+}
+
+function getParameterByName(name) {
+	srchStr=window.location.search;
+	if(srchStr == null || typeof srchStr == 'undefined') {
+	} else {
+		var srchArray=getSearchAsArray(srchStr);
+		if(srchArray[name]) {
+			return srchArray[name];
+		}// else alert("error: no cps information");
+	}
+}
+
+function getSearchAsArray(srchStr) {
+	var results=new Array();
+	var input=unescape(srchStr.substr(1));
+	if(input) {
+		var srchArray=input.split("&");
+		var tempArray=new Array();
+		for(var i=0;i<srchArray.length;i++) {
+			tempArray=srchArray[i].split("=");
+			results[tempArray[0]]=tempArray[1];
+		}
+	}
+	return results;
+}
+
+//finish cps
+if ((SessionObj.Token_Date - curMSecond < 3600000) && (SessionObj.User_ID == null || SessionObj.User_ID == ""))
+	bNeedExtend = true;
+
+/*
+ * jsonp
+ */
+//jsonp 方式
+function generateSessionJSONP(url) {
+	var JSONP=document.createElement("script");  
+    JSONP.type="text/javascript";  
+    JSONP.src=url+"callback=WriteResult";  
+    document.getElementsByTagName("body")[0].appendChild(JSONP);
+    //document.getElementsByTagName("title")[0].innerHTML="sa";
+    
+  //ajax
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.open("GET",JSONP.src,true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			//JSONP.innerHTML=xmlhttp.responseText;
+		}
+	};
+}
+//通用jsonp 方式
+function getJSONP(url,callback) {
+	var JSONP=document.createElement("script");
+    JSONP.type="text/javascript";
+    JSONP.src=url+"&callback="+callback;
+    document.getElementsByTagName("head")[0].appendChild(JSONP);
+}
+
+/*
+ * functions
+ */
 //function:replaceNullOrUndifined
 function replaceNullOrUndifined(data) {
 	if (data == null || typeof data == 'undefined') 
@@ -55,23 +192,8 @@ function replaceNullOrUndifined(data) {
 	return data;
 }
 
-function setCookieFromJSONP(cookieName,cookieValue,timeout) {
+function setCookie_hy(cookieName,cookieValue,timeout) {
 	document.cookie = cookieName+"="+cookieValue+"; expire="+timeout;
-	//var cookie = $.cookie(cookieName, cookieValue, { expires: 1000 * 60 * 5 });
-	/*
-	if($.cookie(cookieName)) {
-		alert("cookie已存在");
-		//getViewData(pageType, id);
-	} else {
-		alert("cookie不存在");
-		// 1分钟过期
-		//var cookie = $.cookie('the_cookie'+id, 'Gonn', { expires: 1000 * 60 * 5 });
-		//$.cookie('the_cookie'+id, 'Gonn');
-		//var cookie = $.cookie('the_cookie'+id);
-		//alert(cookie);
-		//insert_page(pageType, id);
-	}
-	*/
 }
 
 //
@@ -83,7 +205,7 @@ function WriteResult(dataObj) {
 	SessionObj.User_ID = replaceNullOrUndifined(dataObj.User_ID);
 	SessionObj.Token_Date = replaceNullOrUndifined(dataObj.Token_Date);
 	
-	time_out = 1000 * 60 * 5;
+	
 	//获取当前时间
 	date=new Date();
 	expireDays=10;
@@ -92,107 +214,14 @@ function WriteResult(dataObj) {
 	timeout = date.toGMTString();
 	//将cookie设置为10天后过期
 	
-	setCookieFromJSONP('Session_ID',SessionObj.Session_ID,timeout);
-	setCookieFromJSONP('Token_ID',SessionObj.Token_ID,timeout);
-	setCookieFromJSONP('User_ID',SessionObj.User_ID,timeout);
-	setCookieFromJSONP('Token_Date',SessionObj.Token_Date,timeout);
-	
-	/*
-	var data = dataObj.result[0];
-	if (dataObj.result){
-		alert(1);
-		SessionObj.Session_ID = replaceNullOrUndifined(data.COOKIE_SESSION_ID);
-		SessionObj.Token_ID = replaceNullOrUndifined(data.COOKIE_TOKEN_ID);
-		SessionObj.User_ID = replaceNullOrUndifined(data.COOKIE_USER_ID);
-		SessionObj.Token_Date = replaceNullOrUndifined(data.COOKIE_TOKEN_DATE);
-		SessionObj.S_Token_ID = replaceNullOrUndifined(data.COOKIE_S_TOKEN_ID);
-		SessionObj.User_IP = replaceNullOrUndifined(data.COOKIE_USER_IP);
-		SessionObj.User_City = replaceNullOrUndifined(data.COOKIE_USER_CITY);
-		SessionObj.User_Name = replaceNullOrUndifined(data.COOKIE_USER_NAME);
-		SessionObj.NickName = replaceNullOrUndifined(data.COOKIE_NICKNAME);
-		SessionObj.User_Level_ID = replaceNullOrUndifined(data.COOKIE_USER_LEVEL_ID);
-		//判断一下引用此js的原文件的url地址，若该地址不在.wangjiu.com域名下。
-		if (document.domain.indexOf(domainName) == -1) {
-			addCookie("COOKIE_SESSION_ID", SessionObj.Session_ID ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_TOKEN_ID", SessionObj.Token_ID ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_S_TOKEN_ID", SessionObj.S_Token_ID ,data.COOKIE_TIMEOUT,true);
-			addCookie("COOKIE_TOKEN_DATE", SessionObj.Token_Date ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_USER_IP", SessionObj.User_IP ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_USER_CITY", SessionObj.User_City ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_USER_ID", SessionObj.User_ID ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_USER_NAME", SessionObj.User_Name ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_NICKNAME", SessionObj.NickName ,data.COOKIE_TIMEOUT);
-			addCookie("COOKIE_USER_LEVEL_ID", SessionObj.User_Level_ID ,data.COOKIE_TIMEOUT);
-		}
+	setCookie_hy('Session_ID',SessionObj.Session_ID,timeout);
+	setCookie_hy('Token_ID',SessionObj.Token_ID,timeout);
+	setCookie_hy('User_ID',SessionObj.User_ID,timeout);
+	setCookie_hy('Token_Date',SessionObj.Token_Date,timeout);
 
-	}
-	*/
 	SessionObj.IsLinkSuccess = true;
 }
 
-var SessionObj = new Object();
-SessionObj.Session_ID = getCookie("Session_ID");
-SessionObj.Token_ID = getCookie("Token_ID");
-SessionObj.Token_Date = getCookie("Token_Date");
-SessionObj.User_ID = getCookie("USER_ID");
-SessionObj.IsLinkSuccess = true;
-/*
-SessionObj.User_Name = getCookie("COOKIE_USER_NAME");
-SessionObj.NickName = getCookie("COOKIE_NICKNAME");
-SessionObj.User_Level_ID = getCookie("COOKIE_USER_LEVEL_ID");
-SessionObj.Token_Date = getCookie("COOKIE_TOKEN_DATE");
-SessionObj.User_IP = getCookie("COOKIE_USER_IP");
-SessionObj.User_City = getCookie("COOKIE_USER_CITY");
-SessionObj.linkData="";
-*/
-
-
-if (SessionObj.Session_ID == null || SessionObj.Session_ID == ""
-		|| SessionObj.Token_ID  == null || SessionObj.Token_ID  == ""
-		|| SessionObj.Token_Date == null || SessionObj.Token_Date == ""
-/*		|| SessionObj.User_IP == null || SessionObj.User_IP == ""
-		|| SessionObj.User_Level_ID == null || SessionObj.User_Level_ID == ""
-		|| bNeedExtend*/
-		) {
-	SessionObj.IsLinkSuccess = false;
-
-	//JSONP
-	//generateSessionJSONP(authenticationUrl+"/api/list/generateSession.jsonp?IS_PHONE=0&a="+ Math.random());
-	//generateSessionJSONP(authenticationUrl+"/generateSessionJSONP.php?a="+ Math.random());
-	generateSessionJSONP(authenticationUrl+"/generateSessionJSONP.php?");
-}
-
-
-//jsonp 方式
-function generateSessionJSONP(url) {
-	var JSONP=document.createElement("script");  
-    JSONP.type="text/javascript";  
-    JSONP.src=url+"callback=WriteResult";  
-    document.getElementsByTagName("body")[0].appendChild(JSONP);
-    //document.getElementsByTagName("title")[0].innerHTML="sa";
-    
-  //ajax
-	var xmlhttp;
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	} else {// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.open("GET",JSONP.src,true);
-	xmlhttp.send();
-	xmlhttp.onreadystatechange=function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			//JSONP.innerHTML=xmlhttp.responseText;
-		}
-	};
-}
-//通用jsonp 方式
-function getJSONP(url,callback) {
-	var JSONP=document.createElement("script");
-    JSONP.type="text/javascript";
-    JSONP.src=url+"&callback="+callback;
-    document.getElementsByTagName("head")[0].appendChild(JSONP);
-}
 
 /*
 if (SessionObj.Session_ID == null || SessionObj.Session_ID == ""
